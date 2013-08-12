8fd1d80488b90a5834e6e9957ffc93d6878ea5dd
Ryan RC <murphyslaw480@gmail.com>
2013-08-12 11:17:37 -0400

    cryo nade weapon implementation

--- SpaceGame/SpaceGame/states/Level.cs
+++ SpaceGame/SpaceGame/states/Level.cs
@@ -74,8 +74,8 @@ namespace SpaceGame.states
                 _waves[i + data.TrickleWaveData.Length] = new Wave(data.BurstWaveData[i], false, _levelBounds);
             }
             //Test code to set weapons 1-6 to created weapons
-            im.setPrimaryWeapon(new HookShot(_player));
-            im.setSecondaryWeapon(new ProjectileWeapon("Shotgun", _player));
+            im.setPrimaryWeapon(new ProjectileWeapon("Rocket", _player));
+            im.setSecondaryWeapon(new ProjectileWeapon("Cryonade", _player));
             im.setPrimaryGadget(new Gadget("Teleporter", this));
 
             //Set Weapon holders in level
