381aec959f7d479a1be58c24dab99e92fbf8074b
Ryan RC <rcorrent@ufl.edu>
2013-11-07 16:48:34 -0500

    add obstacle class

---
+++ SpaceGame/units/Obstacle.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SpaceGame.units
+{
+    class Obstacle : PhysicalBody
+    {
+        public Obstacle(PhysicalData pd)
+            :base(pd, graphics.Sprite.SpriteType.Obstacle)
+        {
+        }
+    }
+}
