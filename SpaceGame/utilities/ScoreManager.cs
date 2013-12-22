242c4888e92a57e93f407d58d0283cdf6c3b9183
Ryan <murphyslaw480@gmail.com>
2013-12-22 14:16:16 -0500

    scoremanager dataloading and management

--- SpaceGame/utilities/ScoreManager.cs
+++ SpaceGame/utilities/ScoreManager.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
 using Microsoft.Xna.Framework.Graphics;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 
@@ -19,15 +21,29 @@ namespace SpaceGame.utilities
     class ScoreManager
     {
         const int c_maxScorePops = 20;  //Max # of on-screen score popups
+        const string c_scoreFont = "MenuFont";
 
-        static ScoreManager _instance;
+        static ScoreManager s_instance;
+        static SpriteFont s_scoreFont;
+        static ScoreData[] s_scoreData;
+        public static ContentManager Content;
 
         ScorePopup[] _scorePopups;
         public int CurrentScore { get; private set; }
 
+        public static ScoreManager()
+        {
+            s_scoreFont = Content.Load<SpriteFont>(c_scoreFont);
+            s_scoreData = new ScoreData[Enum.GetValues(typeof(ScoreType)).Length];
+            foreach (int val in Enum.GetValues(typeof(ScoreType)))
+            {   //populate score data table from data manager for fast lookup (index by enum values)
+                s_scoreData[val] = DataManager.GetData<ScoreData>(Enum.GetName(typeof(ScoreType), val));
+            }
+        }
+
         public ScoreManager()
         {
-            _instance = this;
+            s_instance = this;
             _scorePopups = new ScorePopup[c_maxScorePops];
             CurrentScore = 0;
         }
@@ -38,6 +54,10 @@ namespace SpaceGame.utilities
         /// <param name="time">elapsed time</param>
         public void Update(TimeSpan time)
         {
+            for (int i = 0; i < _scorePopups.Length; i++)
+            {
+                _scorePopups[i].Update(time);
+            }
         }
 
         /// <summary>
@@ -46,6 +66,10 @@ namespace SpaceGame.utilities
         /// <param name="sb">SpriteBatch with which to draw</param>
         public void Draw(SpriteBatch sb)
         {
+            for (int i = 0; i < _scorePopups.Length; i++)
+            {
+                _scorePopups[i].Draw(sb);
+            }
         }
 
         /// <summary>
@@ -55,14 +79,57 @@ namespace SpaceGame.utilities
         /// <param name="type">type of score event</param>
         public static void RegisterScore(Vector2 position, ScoreType type)
         {
+            s_instance.registerScore(position, type);
+        }
+
+        void registerScore(Vector2 position, ScoreType type)
+        {
+            for (int i = 0; i < _scorePopups.Length; i++)
+            {
+                if (_scorePopups[i].LifeTime <= TimeSpan.Zero)
+                {
+                    _scorePopups[i] = new ScorePopup(position, s_scoreData[(int)type]);
+                }
+            }
         }
 
         struct ScorePopup
         {
             public TimeSpan LifeTime;
             public Vector2 Position;
-            public string Text;
+            public ScoreData Data;
+            public Color TextColor;
+
+            public ScorePopup(Vector2 position, ScoreData data)
+            {
+                Position = position;
+                Data = data;
+                LifeTime = TimeSpan.FromSeconds(data.LifeTime);
+            }
+
+            public void Update(TimeSpan time)
+            {
+                LifeTime -= time;
+            }
+
+            public void Draw(SpriteBatch sb)
+            {
+                if (LifeTime > TimeSpan.Zero)
+                {
+                    sb.DrawString(c_scoreFont, Data.Text, Position, TextColor);
+                }
+            }
         }
+    }
+
+    class ScoreData
+    {
+        public string Name;
+        public string Text;
+        public int Points;
+        public Color Color;
+        public float LifeTime;
+        public float Scale;
     }
 
 }
